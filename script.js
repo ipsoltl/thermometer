abf26838b7bf9df9c15c1a49b1dac8d88f0cae49
ipsoltl <ipsoltl@gmail.com>
2017-12-10 23:14:33 +0300

    Установлен выподающий список

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 //Перевод градусов Фаренгейта в градусы Цельсия и наоборот
+function fun1() {
+  var sel = document.getElementById('mySelect').selectedIndex;
+  var options = document.getElementById('mySelect').options;
+  
+  if(options[sel].text == 'Фаренгейт') {
+    fahrenheit();
+  }else if(options[sel].text == 'Цельсия') {
+    celsius();
+  };  
+};
+
+
 
 function fahrenheit() {
   var num1, result
